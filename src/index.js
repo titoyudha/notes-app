a97a7271afdcdd1187e4974474f3edc9aa3e0f54
Tito Yudha <tito.yudha.bimashika@gmail.com>
2021-01-29 11:18:48 +0700

    add firebase config

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './containers/pages/App';
 import reportWebVitals from './reportWebVitals';
+import firebase from './config/firabase'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+console.log('config firebase ==>',firebase);
+
+
+ReactDOM.render(<App />,document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
